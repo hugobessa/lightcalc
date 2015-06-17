99c3880afa4f4e4a0f47737771e95b910ba81305
Jessé Ferreira Filho <jfnf@cin.ufpe.br>
2015-06-17 00:56:42 -0300

    test green

--- test/unit/controllers/CalculatorsController.test.js
+++ test/unit/controllers/CalculatorsController.test.js
@@ -16,19 +16,20 @@ describe('CalculatorsController', function() {
   describe('Name Of Calculator', function() {
     it('should return error trying to save duplicate username ', function (done) {
 
-		var user = {name: 'BobESPONJA'};
+		var user = {name: 'FantasmaOpera'};
 
       request(sails.hooks.http.app)
         .post('/calculator')
         .send(user)
-        .end(function(err, res) {
-          if (err) {
-            throw res.expect(400,err);
-          }
-          // this is should.js syntax, very clear
-          res.should.have.status(400);
-          done();
-        });
+        .expect(400, done);
+        // .end(function(err, res) {
+        //   if (err) {
+        //     throw err;
+        //   }
+        //   // this is should.js syntax, very clear
+        //   res.should.have.status(400);
+        //   done();
+        // });
         
     });
   });
